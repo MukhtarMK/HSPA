a059e419e273a976676e4b8e70c337e2f47524f4
hpMuk <hp@MUK-PC>
2024-12-18 12:10:20 +0300

    initial Project

---
+++ frontend/src/app/property-List/property-List.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-property-List',
+  templateUrl: './property-List.component.html',
+  styleUrls: ['./property-List.component.css']
+})
+export class PropertyListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ frontend/src/app/property/property-card/property-card.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-property-card',
+  templateUrl: './property-card.component.html',
+  styleUrls: ['./property-card.component.css']
+})
+export class PropertyCardComponent {
+Property:any={
+  "id":1,
+  "Type":"House",
+  "Name":"Mukhtar House",
+  "price":12000
+
+}
+
+}
